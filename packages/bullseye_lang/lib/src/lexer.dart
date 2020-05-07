841dc461d0047b89736f625a800c8e41f91a60ba
Tobe O <thosakwe@gmail.com>
2020-05-07 14:51:19 -0400

    Strings done

--- packages/bullseye_lang/lib/src/lexer.dart
+++ packages/bullseye_lang/lib/src/lexer.dart
@@ -1,13 +1,18 @@
 import 'dart:collection';
 import 'package:bullseye_lang/bullseye_lang.dart';
+import 'package:collection/collection.dart';
 import 'package:string_scanner/string_scanner.dart';
 
 enum LexerState {
   normal,
   doubleQuote,
   singleQuote,
+  rawDoubleQuote,
+  rawSingleQuote,
   tripleDoubleQuote,
   tripleSingleQuote,
+  rawTripleDoubleQuote,
+  rawTripleSingleQuote,
 }
 
 class Lexer {
@@ -86,25 +91,34 @@ class Lexer {
     // String delimiters
     '"': TokenType.DOUBLE_QUOTE,
     "'": TokenType.SINGLE_QUOTE,
+    'r"': TokenType.RAW_DOUBLE_QUOTE,
+    "r'": TokenType.RAW_SINGLE_QUOTE,
     '"""': TokenType.TRIPLE_DOUBLE_QUOTE,
     "'''": TokenType.TRIPLE_SINGLE_QUOTE,
+    'r"""': TokenType.RAW_TRIPLE_DOUBLE_QUOTE,
+    "r'''": TokenType.RAW_TRIPLE_SINGLE_QUOTE,
   };
 
-  static final Map<Pattern, TokenType> genericStringPatterns = {
+  static final Map<Pattern, TokenType> escapePatterns = {
     RegExp(r'\$[A-Za-z_$][A-Za-z0-9_$]*'): TokenType.ESCAPED_ID,
     RegExp(r'\\[bfnrt]'): TokenType.ESCAPE_SEQUENCE,
     RegExp(r'\\x([A-Fa-f0-9][A-Fa-f0-9])'): TokenType.ESCAPE_HEX,
     RegExp(r'\\u{([A-Fa-f0-9]+)}'): TokenType.ESCAPE_UNICODE,
-    '\${': TokenType.DOLLAR_LCURLY,
     '\\\$': TokenType.ESCAPE_DOLLAR,
   };
+
+  static final Map<Pattern, TokenType> genericStringPatterns =
+      Map.of(escapePatterns)
+        ..addAll({
+          '\${': TokenType.DOLLAR_LCURLY,
+        });
 
   static final Map<Pattern, TokenType> doubleQuotePatterns =
       Map.of(genericStringPatterns)
         ..addAll({
           '\\"': TokenType.ESCAPE_DOUBLE_QUOTE,
           '"': TokenType.DOUBLE_QUOTE,
-          RegExp(r'[^"]+'): TokenType.TEXT,
+          RegExp(r'[^"$]+'): TokenType.TEXT,
         });
 
   static final Map<Pattern, TokenType> singleQuotePatterns =
@@ -112,19 +126,51 @@ class Lexer {
         ..addAll({
           "\\'": TokenType.ESCAPE_SINGLE_QUOTE,
           "'": TokenType.SINGLE_QUOTE,
-          RegExp(r"[^']+"): TokenType.TEXT,
+          RegExp(r"[^'$]+"): TokenType.TEXT,
         });
 
-  static final Map<Pattern, TokenType> tripleDoubleQuotePatterns = {
-    '\\"""': TokenType.ESCAPE_TRIPLE_DOUBLE_QUOTE,
-    '"""': TokenType.TRIPLE_DOUBLE_QUOTE,
-    RegExp(r'.+'): TokenType.TEXT,
+  static final Map<Pattern, TokenType> rawDoubleQuotePatterns = {
+    '\\"': TokenType.ESCAPE_DOUBLE_QUOTE,
+    '"': TokenType.DOUBLE_QUOTE,
+    RegExp(r'[^"]+'): TokenType.TEXT,
   };
 
-  static final Map<Pattern, TokenType> tripleSingleQuotePatterns = {
+  static final Map<Pattern, TokenType> rawSingleQuotePatterns = {
+    "\\'": TokenType.ESCAPE_SINGLE_QUOTE,
+    "'": TokenType.SINGLE_QUOTE,
+    RegExp(r"[^']+"): TokenType.TEXT,
+  };
+
+  static final Map<Pattern, TokenType> tripleDoubleQuotePatterns =
+      Map.of(genericStringPatterns)
+        ..addAll({
+          '\\"""': TokenType.ESCAPE_TRIPLE_DOUBLE_QUOTE,
+          '"""': TokenType.TRIPLE_DOUBLE_QUOTE,
+          RegExp(r"[^'$]+"): TokenType.TEXT,
+        });
+
+  static final Map<Pattern, TokenType> tripleSingleQuotePatterns =
+      Map.of(genericStringPatterns)
+        ..addAll({
+          "\\'''": TokenType.ESCAPE_TRIPLE_SINGLE_QUOTE,
+          "'''": TokenType.TRIPLE_SINGLE_QUOTE,
+          RegExp(r"[^'$]+"): TokenType.TEXT,
+        });
+
+  static final Map<Pattern, TokenType> rawTripleDoubleQuotePatterns = {
+    '\\"""': TokenType.ESCAPE_TRIPLE_DOUBLE_QUOTE,
+    '"""': TokenType.TRIPLE_DOUBLE_QUOTE,
+    '""': TokenType.TEXT,
+    '"': TokenType.TEXT,
+    RegExp(r'[^"]+'): TokenType.TEXT,
+  };
+
+  static final Map<Pattern, TokenType> rawTripleSingleQuotePatterns = {
     "\\'''": TokenType.ESCAPE_TRIPLE_SINGLE_QUOTE,
     "'''": TokenType.TRIPLE_SINGLE_QUOTE,
-    RegExp(r'.+'): TokenType.TEXT,
+    "''": TokenType.TEXT,
+    "'": TokenType.TEXT,
+    RegExp(r"[^']+"): TokenType.TEXT,
   };
 
   Iterable<Token> produceTokens() sync* {
@@ -142,7 +188,7 @@ class Lexer {
 
     while (!scanner.isDone) {
       var tokens = <Token>[];
-      if (scanner.scan(whiteSpace)) {
+      if (states.first == LexerState.normal && scanner.scan(whiteSpace)) {
         continue;
       } else {
         Map<Pattern, TokenType> patterns;
@@ -163,6 +209,14 @@ class Lexer {
             patterns = singleQuotePatterns;
             sentinel = TokenType.SINGLE_QUOTE;
             break;
+          case LexerState.rawDoubleQuote:
+            patterns = rawDoubleQuotePatterns;
+            sentinel = TokenType.DOUBLE_QUOTE;
+            break;
+          case LexerState.rawSingleQuote:
+            patterns = rawSingleQuotePatterns;
+            sentinel = TokenType.SINGLE_QUOTE;
+            break;
           case LexerState.tripleDoubleQuote:
             patterns = tripleDoubleQuotePatterns;
             sentinel = TokenType.TRIPLE_DOUBLE_QUOTE;
@@ -174,6 +228,18 @@ class Lexer {
             sentinel = TokenType.TRIPLE_SINGLE_QUOTE;
             lookForTripleQuote = true;
             tripleQuote = "'''";
+            break;
+          case LexerState.rawTripleDoubleQuote:
+            patterns = rawTripleDoubleQuotePatterns;
+            sentinel = TokenType.TRIPLE_DOUBLE_QUOTE;
+            lookForTripleQuote = true;
+            tripleQuote = '"""';
+            break;
+          case LexerState.rawTripleSingleQuote:
+            patterns = rawTripleSingleQuotePatterns;
+            sentinel = TokenType.TRIPLE_SINGLE_QUOTE;
+            lookForTripleQuote = true;
+            tripleQuote = '"""';
             break;
         }
 
@@ -193,7 +259,8 @@ class Lexer {
           scanner.readChar();
         } else {
           flush();
-          tokens.sort((a, b) => b.span.length.compareTo(a.span.length));
+          mergeSort<Token>(tokens,
+              compare: (a, b) => b.span.length.compareTo(a.span.length));
           scanner.position += tokens.first.span.length;
           yield tokens.first;
 
@@ -210,13 +277,25 @@ class Lexer {
                     states.addFirst(LexerState.doubleQuote);
                     break;
                   case TokenType.SINGLE_QUOTE:
-                    states.addFirst(LexerState.doubleQuote);
+                    states.addFirst(LexerState.singleQuote);
+                    break;
+                  case TokenType.RAW_DOUBLE_QUOTE:
+                    states.addFirst(LexerState.rawDoubleQuote);
+                    break;
+                  case TokenType.RAW_SINGLE_QUOTE:
+                    states.addFirst(LexerState.rawSingleQuote);
                     break;
                   case TokenType.TRIPLE_DOUBLE_QUOTE:
                     states.addFirst(LexerState.tripleDoubleQuote);
                     break;
                   case TokenType.TRIPLE_SINGLE_QUOTE:
                     states.addFirst(LexerState.tripleSingleQuote);
+                    break;
+                  case TokenType.RAW_TRIPLE_DOUBLE_QUOTE:
+                    states.addFirst(LexerState.rawTripleDoubleQuote);
+                    break;
+                  case TokenType.RAW_TRIPLE_SINGLE_QUOTE:
+                    states.addFirst(LexerState.rawTripleSingleQuote);
                     break;
                   default:
                     break;
