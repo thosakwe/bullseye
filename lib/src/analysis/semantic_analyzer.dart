a8e92f061cc63a1e20e1226dfb5b17e50dc8d446
Tobe O <thosakwe@gmail.com>
2021-07-07 10:02:49 -0400

    Add stub for analyzer class

---
+++ lib/src/analysis/semantic_analyzer.dart
@@ -0,0 +1,5 @@
+import 'package:bullseye_lang/src/analysis/value.dart';
+import 'package:bullseye_lang/src/ast/expr.dart';
+
+class SemanticAnalyzer implements ExprVisitor<BullseyeValue> {
+}
--- lib/src/ast/expr.dart
+++ lib/src/ast/expr.dart
@@ -4,4 +4,9 @@ import 'node.dart';
 
 abstract class Expr extends Node {
   Expr(FileSpan span) : super(span);
+
+  T accept<T>(ExprVisitor<T> visitor);
+}
+
+abstract class ExprVisitor<T> {
 }
