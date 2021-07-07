046a0d9402b869eb3fe07ffb4d97eeaf3dd207f8
Tobe O <thosakwe@gmail.com>
2021-07-07 10:46:32 -0400

    constbool visitor

--- lib/src/analysis/value_visitor.dart
+++ lib/src/analysis/value_visitor.dart
@@ -1,21 +1,22 @@
 import 'value.dart';
 
 abstract class ValueVisitor<T> {
+  T visitAwait(Await node);
+  T visitBinaryOperation(BinaryOperation node);
+  T visitBullseyeFunction(BullseyeFunction node); 
+  T visitClassInit(ClassInit node);
+  T visitConstBool(ConstBool node);
   T visitConstDouble(ConstDouble node);
   T visitConstInt(ConstInt node);
   T visitConstString(ConstString node);
   T visitConstUnit(ConstUnit node);
-  T visitTuple(Tuple node);
-  T visitIfThen(IfThen node);
   T visitFunctionCall(FunctionCall node);
-  T visitLetIn(LetIn node);
-  T visitBinaryOperation(BinaryOperation node);
-  T visitBullseyeFunction(BullseyeFunction node); 
-  T visitAwait(Await node);
-  T visitIOBind(IOBind node);
-  T visitWrapPureInIO(WrapPureInIO node);
-  T visitClassInit(ClassInit node);
-  T visitTaggedSumInit(TaggedSumInit node);
   T visitGetSymbol(GetSymbol node);
+  T visitIOBind(IOBind node);
+  T visitIfThen(IfThen node);
+  T visitLetIn(LetIn node);
   T visitSetSymbol(SetSymbol node);
+  T visitTaggedSumInit(TaggedSumInit node);
+  T visitTuple(Tuple node);
+  T visitWrapPureInIO(WrapPureInIO node);
 }
