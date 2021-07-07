6bbfac74d1d1d8693b4116c54c68162606a94be3
Tobe O <thosakwe@gmail.com>
2021-07-07 10:14:56 -0400

    Add value visitor

---
+++ lib/src/analysis/value_visitor.dart
@@ -0,0 +1,21 @@
+import 'value.dart';
+
+abstract class BullseyeValueVisitor<T> {
+  T visitConstDouble(ConstDouble node);
+  T visitConstInt(ConstInt node);
+  T visitConstString(ConstString node);
+  T visitConstUnit(ConstUnit node);
+  T visitTuple(Tuple node);
+  T visitIfThen(IfThen node);
+  T visitFunctionCall(FunctionCall node);
+  T visitLetIn(LetIn node);
+  T visitBinaryOperation(BinaryOperation node);
+  T visitBullseyeFunction(BullseyeFunction node); 
+  T visitAwait(Await node);
+  T visitIOBind(IOBind node);
+  T visitWrapPureInIO(WrapPureInIO node);
+  T visitClassInit(ClassInit node);
+  T visitTaggedSumInit(TaggedSumInit node);
+  T visitGetSymbol(GetSymbol node);
+  T visitSetSymbol(SetSymbol node);
+}
