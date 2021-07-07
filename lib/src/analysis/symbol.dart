e60cd06dad23fd5a67d9e0732b8722cf440fdd20
Tobe O <thosakwe@gmail.com>
2021-07-07 09:54:43 -0400

    Add types to symbols

--- lib/src/analysis/symbol.dart
+++ lib/src/analysis/symbol.dart
@@ -1,9 +1,20 @@
+import 'type.dart';
+import 'type_provider.dart';
+import 'value.dart';
+
 abstract class BullseyeSymbol {
+  BullseyeType getType(TypeProvider typeProvider);
 }
 
 // TODO(thosakwe): types, constructors, modules(?), etc.
 
 /// A function parameter...
 class Parameter extends BullseyeSymbol {
+  final String name;
+  final BullseyeType type;
+  final BullseyeValue? defaultsTo;
+  Parameter(this.name, this.type, this.defaultsTo);
 
+  @override
+  BullseyeType getType(TypeProvider typeProvider) => type;
 }
