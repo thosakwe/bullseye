30173edc9d26527ee0db5e2600f0c6ba2fd37308
Tobe O <thosakwe@gmail.com>
2021-07-11 14:26:09 -0400

    Add FunctionTargetVisitor stubs

--- lib/src/analysis/function_target_visitor.dart
+++ lib/src/analysis/function_target_visitor.dart
@@ -1,2 +1,8 @@
+import 'value.dart';
+
 abstract class FunctionTargetVisitor<T> {
+  T visitDirectTarget(DirectTarget node);
+  T visitIndirectTarget(IndirectTarget node);
+  T visitPartialTarget(PartialTarget node);
+  T visitConstructor(Constructor node);
 }
