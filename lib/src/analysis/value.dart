fac96fe1ea5550a6cccaf398e803d320f02fc9ba
Tobe O <thosakwe@gmail.com>
2021-07-11 14:37:07 -0400

    Implement FunctionTarget methods

--- lib/src/analysis/value.dart
+++ lib/src/analysis/value.dart
@@ -126,22 +126,28 @@ class DirectTarget extends FunctionTarget {
   final Symbol target;
 
   DirectTarget(this.target);
+
+  T accept<T>(FunctionTargetVisitor<T> visitor) => visitor.visitDirectTarget(this);
 }
 
 class IndirectTarget extends FunctionTarget {
   final BullseyeValue target;
 
   IndirectTarget(this.target);
+
+  T accept<T>(FunctionTargetVisitor<T> visitor) => visitor.visitIndirectTarget(this);
 }
 
 class PartialTarget extends FunctionTarget {
   final BullseyeValue target;
 
   PartialTarget(this.target);
+
+  T accept<T>(FunctionTargetVisitor<T> visitor) => visitor.visitPartialTarget(this);
 }
 
 class Constructor extends FunctionTarget {
-
+  T accept<T>(FunctionTargetVisitor<T> visitor) => visitor.visitConstructor(this);
 }
 
 class LetIn extends BullseyeValue {
