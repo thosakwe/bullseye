94174bc85571bc58a3bd3e59cca83b4f0c3bcc7b
Tobe O <thosakwe@gmail.com>
2021-07-11 19:53:41 -0400

    Comments for TypeProvider

--- lib/src/analysis/type_provider.dart
+++ lib/src/analysis/type_provider.dart
@@ -1,14 +1,29 @@
 import 'type.dart';
 
+/// A class that manages singletons of common Bullseye types.
 class TypeProvider {
+  /// The `bool type.
   final BoolType boolType = BoolType();
+
+  /// The `double type.
   final DoubleType doubleType = DoubleType();
+
+  /// The `IO ()` type.
   final IOType ioUnitType = IOType(UnitType());
+
+  /// The `int` type.
   final IntType intType = IntType();
+
+  /// The `String` type.
   final StringType stringType = StringType();
+
+  /// The `()` type.
   final UnitType unitType = UnitType();
 
+  /// The `Either` type.
   PolymorphicType get eitherType => _eitherType;
+
+  /// The `Maybe` type.
   PolymorphicType get maybeType => _maybeType;
 
   late PolymorphicType _eitherType, _maybeType;
